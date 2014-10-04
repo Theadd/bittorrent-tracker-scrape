2dbece4f266268b6f9a68f952f45ee0b69a05622
Theadd <pantallazo@gmail.com>
2014-10-04 07:30:16 +0200

    fix

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ var Scrape = require('./scraper')
 var Update = require('./updater')
 var Torrent = require('./torrent')
 
-var workerFarm = require('worker-farm')
-  , workers    = workerFarm(require.resolve('./torrent'))
-
 module.exports = ScrapeWorker
 module.exports.Scrape = Scrape
 module.exports.Update = Update
@@ -13,6 +10,9 @@ module.exports.Torrent = Torrent
 function ScrapeWorker (hash, announce, opts, callback) {
   announce = announce || []
   opts = opts || {}
+
+  var workerFarm = require('worker-farm')
+    , workers    = workerFarm(require.resolve('./torrent'))
 
   workers(hash, announce, opts, function(err,  res) {
     callback(err, res)
