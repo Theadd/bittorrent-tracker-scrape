2538bfe0cc0aa8bf672baf41b94a7e63c005ff01
Theadd <pantallazo@gmail.com>
2015-02-27 22:28:59 +0100

    metadata

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var Scrape = require('./scraper')
 var Update = require('./updater')
 var Torrent = require('./torrent')
+var Metadata = require('./metadata')
 
 module.exports = ScrapeWorker
 module.exports.Scrape = Scrape
 module.exports.Update = Update
 module.exports.Torrent = Torrent
+module.exports.Metadata = Metadata
 
 function ScrapeWorker (hash, announce, opts, callback) {
   announce = announce || []
