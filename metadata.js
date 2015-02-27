d942fd1283680251a972f06563db8587fe7a4050
Theadd <pantallazo@gmail.com>
2015-02-27 23:11:04 +0100

    clean

--- metadata.js
+++ metadata.js
@@ -33,10 +33,11 @@ function Metadata (target, opts, callback) {
         return "magnet:?xt=urn:btih:" + info_hash.toLowerCase()
       }
     },
-    priority: [ "torcache.net", "zoink.it", /*"torrage.com", "reflektor.karmorra.info",*/ "magnet" ],
+    priority: [ "torcache.net", "zoink.it", /*"torrage.com",*/ "reflektor.karmorra.info", "magnet" ],
     targetInputFirst: true,
     useSources: true,
-    disableMagnet: false
+    disableMagnet: false,
+    outputAsList: false
   }, opts || {})
 
   if (typeof callback === "function") {
@@ -45,10 +46,6 @@ function Metadata (target, opts, callback) {
 }
 
 Metadata.prototype._parseInput = function (input) {
-  // http://torrage.com/torrent/948BB2B15ACF6CC55B00BADF0AE171D2A6773605.torrent
-  // magnet:?xt=urn:btih:948bb2b15acf6cc55b00badf0ae171d2a6773605&dn=Vikings%20S03E01%20HDTV%20x264-KILLERS...&tr=http%3A%2F%2Ftorrent.gresille.org%2Fannounce&tr=http%3A%2F%2Fwww.eddie4.nl%3A6969%2Fannounce&tr=http%3A%2F%2Ftracker.trackerfix.com%2Fannounce&tr=http%3A%2F%2Fbttracker.crunchbanglinux.org%3A6969%2Fannounce&tr=http%3A%2F%2Fmgtracker.org%3A2710%2Fannounce&tr=http%3A%2F%2Fretracker.uln-ix.ru%2Fannounce&tr=http%3A%2F%2Ftracker.best-torrents.net%3A6969%2Fannounce&tr=http%3A%2F%2Fflashtorrents.org%3A6969%2Fannounce&tr=http%3A%2F%2Ftracker.tfile.me%2Fannounce&tr=http%3A%2F%2Ftracker.flashtorrents.org%3A6969%2Fannounce%EF%BF%BD%EF%BF%BD%EF%BF%BD%EF%BF%BD%EF%BF%BD%EF%BF%BD%EF%BF%BD%EF%BF%BD%EF%BF%BD%EF%BF%BD%EF%BF%BD%EF%BF%BDc%EF%BF%BD%EF%BF%BD%EF%BF%BD%EF%BF%BD%EF%BF%BD%EF%BF%BD%EE%83%BF&tr=http%3A%2F%2Fannounce.xxx-tracker.com%3A2710%2Fannounce&tr=http%3A%2F%2Fbt.careland.com.cn%3A6969%2Fannounce&tr=http%3A%2F%2Fcastradio.net%3A6969%2Fannounce&tr=http%3A%2F%2Fretracker.adminko.org%2Fannounce&tr=http%3A%2F%2Fretracker.telecom.kz%2Fannounce&tr=http%3A%2F%2Ftracker.flashtorrents.org%3A6969%2Fannounce%3F%3F%3F%3F%3F%3F%3F%3F%3F%3F%3F%3Fc&tr=http%3A%2F%2Ftracker.flashtorrents.org%3A6969%2Fannounce&tr=http%3A%2F%2Fserver1.9sheng.cn%3A6969%2Fannounce&tr=http%3A%2F%2Ftracker.dler.org%3A6969%2Fannounce&tr=http%3A%2F%2Ftracker2.wasabii.com.tw%3A6969%2Fannounce
-  // 948BB2B15ACF6CC55B00BADF0AE171D2A6773605
-
   var self = this
 
   self._info_hash = false
@@ -106,39 +103,46 @@ Metadata.prototype.request = function (callback) {
 
   self._buildQueue()
 
-  console.log("\n---------------- QUEUE -------------------")
-  console.log(JSON.stringify(self._queue, null, "  "))
-  console.log("-----------------------------------------\n")
-
   if (self._queue.length) {
-    var item = self._queue.shift()
-    self._request(item, callback)
+    var item = self._queue.shift(),
+      list = []
+
+    self._request(item, list, callback)
   } else {
-    callback({
-      "error": new Error ("Queue is empty.")
-    })
+    return callback(new Error ("Queue is empty."))
   }
 }
 
-Metadata.prototype._request = function (item, callback) {
+Metadata.prototype._request = function (item, list, callback) {
   var self = this
 
   self._get(item, function (err, res) {
-    console.log("\n... in callback of _get from _request for " + item.source + ", err:")
-    console.log(err)
     if (res) {
-      console.log(">>>>> RESULTS FOUND!")
-      self.store(item.source, res)
+      if (!self._opts.outputAsList) {
+        return callback(null, res)
+      } else {
+        list.push(res)
+      }
+    } else {
+      if (self._opts.outputAsList) {
+        list.push({
+          "source": item.source,
+          "link": item.link,
+          "error": err
+        })
+      }
     }
-    console.log("   ")
     if (self._queue.length) {
       var nextItem = self._queue.shift()
-      self._request(nextItem, callback)
+      self._request(nextItem, list, callback)
     } else {
-      callback(new Error ("Reached end of the queue with no luck."))
+      if (!self._opts.outputAsList) {
+        return callback(new Error ("Invalid response from sources."))
+      } else {
+        return callback(null, list)
+      }
     }
   })
-
 }
 
 Metadata.prototype._get = function (item, callback) {
@@ -158,44 +162,21 @@ Metadata.prototype._get = function (item, callback) {
 Metadata.prototype._getFile = function (item, callback) {
   readTorrent(item.link, function(err, torrent) {
     if (err) {
-      console.log("\n..... ERROR RETRIEVING METADATA FROM " + item.link + "\n")
-      return callback(new Error ("Error in " + item.source + "; " + err))
-      /*console.log("\n..... ERROR RETRIEVING METADATA FROM " + item.link + "\n")
-      if (self._queue.length) {
-        var nextItem = self._queue.shift()
-        self._request(nextItem, callback)
-      } else {
-        callback(new Error ("Reached end of the queue with no luck."))
-      }*/
+      return callback(new Error ("Error in " + item.source + ": " + err))
     } else {
-      //self.store(item.source, torrent)
-
-      /*if (self._queue.length) {
-        var nextItem2 = self._queue.shift()
-        self._request(nextItem2, callback)
-      } else {
-        callback(new Error ("Reached end of the queue with no luck."))
-      }*/
       return callback(null, {
        "source": item.source,
        "link": item.link,
        "data": torrent
       })
     }
-    console.log("+++++++++++++++++++++++++++++++++++")
   })
 }
 
 Metadata.prototype._getMagnet = function (item, callback) {
-  var self = this,
-    client = new webTorrent()
+  var client = new webTorrent()
 
   client.add(item.link, function (torrent) {
-    // Got torrent metadata!
-    console.log("\n========= WebTorrent ===========")
-    console.log('Torrent info hash:', torrent.infoHash)
-    //console.log(torrent)
-    self.store(item.source, torrent.parsedTorrent)
     return client.destroy(function () {
       return callback(null, {
         "source": item.source,
@@ -203,12 +184,5 @@ Metadata.prototype._getMagnet = function (item, callback) {
         "data": torrent.parsedTorrent
       })
     })
-
   })
-}
-
-
-Metadata.prototype.store = function (source, torrent) {
-  var fs = require('fs')
-  fs.writeFileSync("./data/" + source + ".json", JSON.stringify(torrent, null, "  "))
 }
